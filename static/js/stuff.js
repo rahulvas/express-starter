a34fd36eae9c40857853d778dd827546ab9ac152
Rahul <rvaswani91@gmail.com>
2014-02-19 19:34:36 -0800

    Lab 2 is done!

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -8,17 +8,26 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
+	$('#houdini_text').hide();
   //WRITE CODE HERE
 });
 
 $('#reappear').click(function() {
+	$('#houdini_text').show();
   //WRITE CODE HERE
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
+$('#tickled_button').click(function() {
+	$('#tickled_text').css('color', 'blue');
+});
+
 // WRITE CODE HERE
 
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#name_button').click(function() {
+	alert('Hey there '+ $('#my_name').val() + ', looking good today!');
+});
