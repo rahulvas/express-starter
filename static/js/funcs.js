341974c935b2f3ed050512cc0d320a6f18990a5c
Rahul <rvaswani91@gmail.com>
2014-03-12 20:07:26 -0700

    Finished everything but challenge!

--- static/js/funcs.js
+++ static/js/funcs.js
@@ -28,6 +28,35 @@ $(document).ready(function() {
 
   // Write drawTriplet function here
 
+  var drawTriplet = function(x,y,radius, color) {
+    drawCircle(x-2*radius/3, y, radius, color);
+    drawCircle(x, y-radius, radius, color);
+    drawCircle(x+2*radius/3, y, radius, color);
+  };
+
+  var drawTriangle = function(x,y, sideLen, color) {
+    context.beginPath();
+    context.fillStyle = color;
+    context.moveTo(x,y);
+    context.lineTo(x+ sideLen/2, y - sideLen*Math.sin(Math.PI/3));
+    context.lineTo(x+sideLen, y);
+    context.closePath();
+    context.fill();
+    context.stroke();
+  };
+
+  var drawTriforce = function(x,y,sideLen, color) {
+    drawTriangle(x, y, sideLen, color);
+    drawTriangle(x - x/2, y + sideLen*Math.sin(Math.PI/3), sideLen, color);
+    drawTriangle(x + x/2, y + sideLen*Math.sin(Math.PI/3), sideLen, color);
+  };
+
+  var drawTripleTriforce = function(x,y,sideLen, color) {
+    drawTriforce(x,y,sideLen, color);
+    drawTriforce(x - x/2 ,y+ sideLen*Math.sin(Math.PI/3),sideLen, color);
+    drawTriforce(x + x/2 ,y+ sideLen*Math.sin(Math.PI/3),sideLen, color);
+  };
+
   // Challenge:
   // Write drawTriangle, drawTriforce, drawTripleTriforce,
   // drawSierpinski functions here
@@ -58,6 +87,33 @@ $(document).ready(function() {
     drawCircle(100, 75, 25, 'green');
     drawCircle(125, 100, 25, 'green');
     drawCircle(100, 125, 25, 'green');
+  });
+
+  $('#p6').click(function() {
+    drawTriplet(75, 75, 25, 'green');
+  });
+
+  $('#p7').click(function() {
+    drawTriplet(150, 150, 77, 'blue');
+  });
+
+  $('#p8').click(function() {
+    drawTriplet(75, 75, 25, 'green');
+    drawTriplet(350, 250, 25, 'red');
+    drawTriplet(150, 150, 77, 'blue');
+    drawTriplet(400, 400, 95, 'pink');
+  });
+
+  $('#p9').click(function() {
+    drawTriangle(100, 100, 105, 'blue');
+  });
+
+  $('#p10').click(function() {
+    drawTriforce(100, 100, 105, 'blue');
+  });
+
+  $('#p11').click(function() {
+    drawTripleTriforce(100, 100, 105, 'blue');
   });
 
 });
