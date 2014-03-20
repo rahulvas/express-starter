75f0cdfed42a9598c2a871eb96e8e694a354fcab
Rahul <rvaswani91@gmail.com>
2014-03-19 19:22:16 -0700

    ball is confined and bouncy

--- static/js/ball.js
+++ static/js/ball.js
@@ -11,12 +11,24 @@ $(document).ready(function() {
   circle.x = 20;
   circle.y = 20;
   circle.r = 20;
+  circle.vx = 5;
+  circle.vy = 5;
 
   //run an iteration of the game
   var updateGame = function() {
     
-    circle.x += 5;
-    circle.y += 5;
+    if((circle.vx > 0 && circle.x + circle.r >= canvas.width) || (circle.vx <
+    0 && circle.x <= 0)){
+      circle.vx = -circle.vx; 
+    };
+
+    if((circle.vy > 0 && circle.y + circle.r >= canvas.height) || (circle.vy <
+    0 && circle.y < 0)){
+      circle.vy = -circle.vy; 
+    };
+    
+    circle.x += circle.vx;
+    circle.y += circle.vy;
 
     context.strokeRect(0, 0, canvas.width, canvas.height);
     context.clearRect(0, 0, canvas.width, canvas.height);
