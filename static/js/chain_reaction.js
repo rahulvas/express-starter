4b850c08af2c48e23edde095308f8f4ffeac4cc2
Rahul <rvaswani91@gmail.com>
2014-04-02 18:58:26 -0700

    the balls are now moving

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -11,14 +11,20 @@ $(document).ready(function() {
     b0.x = 40;
     b0.y = 40;
     b0.r = 20;
+    b0.vx = 10;
+    b0.vy = 10;
   var b1 = {};
     b1.x = 200;
     b1.y = 200;
     b1.r = 20;
+    b1.vx = -5;
+    b1.vy = -5;
   var b2 = {};
     b2.x = 100;
     b2.y = 100;
     b2.r = 20;
+    b2.vx = -8;
+    b2.vy = -8;
   balls.push(b0);
   balls.push(b1);
   balls.push(b2);
@@ -26,12 +32,35 @@ $(document).ready(function() {
   // Run an interation of the game
   var updateGame = function() {
     // PUT STUFF HERE
-  for(var i = 0; i<= balls.length; i++){
+
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+  for(var i = 0; i < balls.length; i++){
+    if((balls[i].vx > 0 && balls[i].x + balls[i].r >= canvas.width) || (balls[i].vx <
+    0 && balls[i].x <= 0)){
+      balls[i].vx = -balls[i].vx; 
+    };
+
+    if((balls[i].vy > 0 && balls[i].y + balls[i].r >= canvas.height) || (balls[i].vy <
+    0 && balls[i].y < 0)){
+      balls[i].vy = -balls[i].vy; 
+    };
+    
+    balls[i].x += balls[i].vx;
+    balls[i].y += balls[i].vy;
+}
+
+
+
+  for(var i = 0; i< balls.length; i++){
     context.beginPath();
     context.arc(balls[i].x, balls[i].y, balls[i].r, 0, 2*Math.PI);
     context.closePath();
     context.stroke();
   };
+
+  setTimeout(updateGame, 10)
 
   };
 
@@ -42,6 +71,8 @@ $(document).ready(function() {
     var y = e.pageY - $(this).offset().top;
     // PUT STUFF HERE
   });
+
+
 
   updateGame();
 });
